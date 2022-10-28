459d618ea130c68c3da430edb5a2783dde909577
Minedhs <denis2976996@gmail.com>
2022-10-28 16:24:11 +0300

    First Commit

---
+++ use-class-syntax-to-define-a-constructor-function.js
@@ -0,0 +1,13 @@
+
+
+class Vegetable {
+	constructor(name) {
+		this.name = name;
+	}
+	takeOff() {
+		console.log(carrot.name);
+	}
+}
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name);
